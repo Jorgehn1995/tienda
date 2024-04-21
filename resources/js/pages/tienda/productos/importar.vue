b2a40f4201fb3ab8731e6f4c1c24145dd82ffda1
Jorge Asus <jorgehn1995@gmail.com>
2024-04-21 13:49:48 -0600

    se agrego la visualizacion de importacion

--- resources/js/pages/tienda/productos/importar.vue
+++ resources/js/pages/tienda/productos/importar.vue
@@ -7,12 +7,48 @@
           Importa los productos desde un archivo de excel
         </span>
       </div>
+      <v-card outlined elevation="0" class="rounded-lg">
+        <v-card-title> Productos </v-card-title>
+        <v-card-subtitle>
+          Importa tus productos desde un archivo de excel
+        </v-card-subtitle>
+        <v-card-text>
+          <v-btn
+            color="green"
+            dark
+            link
+            target="_blank"
+            href="/files/plantilla_productos.xlsx"
+          >
+            <v-icon left>mdi-microsoft-excel</v-icon>
+            Descargar Plantilla
+          </v-btn>
+          <v-btn color="primary" class="ml-1">
+            <v-icon left>mdi-cloud-upload</v-icon>
+            Cargar Archivo
+          </v-btn>
+          <div style="height: 250px">
+            <div
+              class="d-flex justify-center alin-center pt-6 flex-column text-center"
+            >
+              <v-icon size="100">mdi-cloud-upload-outline</v-icon>
+              <span class="caption text-h6"
+                >Sube un archivo para procesar los productos</span
+              >
+            </div>
+          </div>
+        </v-card-text>
+      </v-card>
     </v-container>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data: () => ({
+    isLoading: false,
+  }),
+};
 </script>
 
 <style>
